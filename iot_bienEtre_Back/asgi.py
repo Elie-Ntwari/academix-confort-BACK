51a87a03488d66ba0c9f34ac440bed7779268afd
Elie Ntwari <elientwari@gmail.com>
2026-01-14 14:44:47 +0100

    Fix websockett 4

--- iot_bienEtre_Back/asgi.py
+++ iot_bienEtre_Back/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import core
+from core import routing
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iot_bienEtre_Back.settings')
@@ -20,7 +20,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iot_bienEtre_Back.settings')
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(core.routing.websocket_urlpatterns)
+        URLRouter(routing.websocket_urlpatterns)
     ),
 })
 
